7fe25319d1f3b9f4ccd18e7c2cffea7e6e43ad48
Muthu131 <muthukumar@mazeworkssolutions.com>
2024-01-09 16:46:21 +0530

    work muthu css changes

--- jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.js
+++ jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.js
@@ -25,27 +25,30 @@ class AiImageSearchPage {
         frappe.set_route('Form', 'ai-image-details');
     }
 
-	previewImage() {
+	previewImage(imagePath) {
 		console.log("previewImage");
-		// var canvas = document.getElementById('canv1');
-		// var context = canvas.getContext('2d');
-		
-		// Clear the canvas and set the text
-		// context.clearRect(0, 0, canvas.width, canvas.height);
-		// context.fillText('Hello, World!', 10, 50);
-	
-		// // Convert the canvas content to an image data URL
-		// var imageDataURL = canvas.toDataURL('/assets/jkfenner_image_process/images/jk_fenner_background.png');
-	
-		// // Replace the content of the image tag with the data URL
-		// $('#canv1').attr('src', imageDataURL);
+					// var canvas = document.getElementById('canv1');
+					// var context = canvas.getContext('2d');
+					
+					// Clear the canvas and set the text
+					// context.clearRect(0, 0, canvas.width, canvas.height);
+					// context.fillText('Hello, World!', 10, 50);
+				
+					// // Convert the canvas content to an image data URL
+					// var imageDataURL = canvas.toDataURL('/assets/jkfenner_image_process/images/jk_fenner_background.png');
+				
+					// // Replace the content of the image tag with the data URL
+					// $('#canv1').attr('src', imageDataURL);
 
 		var myImg = new Image();
 		myImg.onload = function() {
 		context.drawImage(myImg, 0,0);
+		myImg.style.maxWidth = '50px';
+		document.getElementById('preview-image').appendChild(myImg);
 		};
-		myImg.src = '/assets/jkfenner_image_process/images/E70657-1.jpg';
-		myImg.maxWidth = '50px';
+		// myImg.src = '/assets/jkfenner_image_process/images/E70657-1.jpg';
+		// myImg.maxWidth = '50px';
+		myImg.src = imagePath;
 		$("#preview-image").html(myImg)
 	}
 }
