3848035983bd1e76b4fdc08b142070ada5f40040
TMuthukumar98 <mkumart998@gmail.com>
2024-02-05 21:30:46 +0530

    work muthu tempp

--- jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.py
+++ jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.py
@@ -1,12 +1,12 @@
 import frappe
 import os
-from jkfennerai.inference import predict
+# from jkfennerai.inference import predict
 
 
 
 @frappe.whitelist(allow_guest=True)
 def guess_image(image):
-    ai_responses = {
+    return {
         "images": [
             "/assets/jkfenner_image_process/images/machine_learning/augment_images/E72068/E72068-16.jpg",
             "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70368/C70368-41.jpg",
@@ -31,6 +31,20 @@ def guess_image(image):
             "0.7235214",
             "0.7229657"
         ]
+    }
+    config_file_path = "/home/frappe/frappe-bench/apps/jkfenner_image_process/jkfenner_image_process/config/aiconfig.cfg"
+    _file = frappe.get_doc("File", {'name': image})
+    img_path = _file.get_full_path()
+    predictor = predict(config_file_path)
+    similarity_scores, similarity_images, pad_img = predictor.run(img_path)
+    similarity_images_with_path = []
+    for similarity_image in similarity_images:
+        imagefolder = str(similarity_image).split('-')[0]
+        similarity_images_with_path.append("/assets/jkfenner_image_process/images/machine_learning/augment_images/{}/{}".format(imagefolder, similarity_image))
+    similarity_scores = [str(similarity_score) for similarity_score in similarity_scores]
+    return {
+        'images': similarity_images_with_path,
+        'scores': similarity_scores
     }
     # config_file_path = "/home/frappe/frappe-bench/apps/jkfenner_image_process/jkfenner_image_process/config/aiconfig.cfg"
     # _file = frappe.get_doc("File", {'name': image})
