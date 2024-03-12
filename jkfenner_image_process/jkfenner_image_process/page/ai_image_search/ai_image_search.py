bcd2750a4952c23294664b5fcc7b67d0cefa0883
Muthu131 <muthukumar@mazeworkssolutions.com>
2024-03-12 19:07:13 +0530

    work muthu image search page corrections

--- jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.py
+++ jkfenner_image_process/jkfenner_image_process/page/ai_image_search/ai_image_search.py
@@ -10,32 +10,32 @@ def guess_image(images,inner_diameter_1,inner_diameter_2,length,branched,dark_ba
     images = images.split('~')
     _files = frappe.get_list("File", filters = {'name':["in", images]}, fields=["name"], pluck="name")
     is_branched_hose = True if branched == 'true' else False
-    ai_responses = {
-        "images": [
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/E72068/E72068-16.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70368/C70368-41.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/E71617/E71617-28.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70592/C70592-90.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70477/C70477-49.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/E71617/E71617-74.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/E72068/E72068-61.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70592/C70592-34.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70680/C70680-9.jpg",
-            "/assets/jkfenner_image_process/images/machine_learning/augment_images/A70896/A70896-59.jpg"
-        ],
-        "scores": [
-            "0.8233142",
-            "0.7654208",
-            "0.7642282",
-            "0.7544414",
-            "0.74282396",
-            "0.742217",
-            "0.73571",
-            "0.7337779",
-            "0.7235214",
-            "0.7229657"
-        ]
-    }
+    # ai_responses = {
+    #     "images": [
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/E72068/E72068-16.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70368/C70368-41.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/E71617/E71617-28.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70592/C70592-90.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70477/C70477-49.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/E71617/E71617-74.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/E72068/E72068-61.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70592/C70592-34.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/C70680/C70680-9.jpg",
+    #         "/assets/jkfenner_image_process/images/machine_learning/augment_images/A70896/A70896-59.jpg"
+    #     ],
+    #     "scores": [
+    #         "0.8233142",
+    #         "0.7654208",
+    #         "0.7642282",
+    #         "0.7544414",
+    #         "0.74282396",
+    #         "0.742217",
+    #         "0.73571",
+    #         "0.7337779",
+    #         "0.7235214",
+    #         "0.7229657"
+    #     ]
+    # }
 
     
     ai_responses = {}
@@ -65,8 +65,8 @@ def guess_image(images,inner_diameter_1,inner_diameter_2,length,branched,dark_ba
         similarity_scores.append(score)
     similarity_scores = [str(similarity_score) for similarity_score in similarity_scores]
 
-    # ai_responses["images"] = similarity_images_with_path
-    # ai_responses["scores"] = similarity_scores
+    ai_responses["images"] = similarity_images_with_path
+    ai_responses["scores"] = similarity_scores
     
     predicted_images = ai_responses['images']
     docsinfo = [image.split('/')[-2] for image in predicted_images]
